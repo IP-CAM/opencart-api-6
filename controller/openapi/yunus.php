345d53ecfb929a273c2d3507c2a2b83d1bde6c65
unknown <h.sahin@34p-h-sahin.CMD.feza.media>
2015-03-21 19:20:12 +0200

    isim değişikliği

--- controller/openapi/yunus.php
+++ controller/openapi/yunus.php
@@ -6,6 +6,6 @@ class ControllerOpenapiHello extends AbstractOpenApi
 
     public function index()
     {
-        $this->output(["hello" => "yunus"]);
+        $this->output(["hello" => "bitirim ikili"]);
     }
 }
