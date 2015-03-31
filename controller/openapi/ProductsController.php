1522483f6c0306dbdc48ef8192765245ab30d78c
unknown <h.sahin@34p-h-sahin.CMD.feza.media>
2015-03-31 17:15:56 +0300

    ProductController sayfasının demo hali

--- controller/openapi/ProductsController.php
+++ controller/openapi/ProductsController.php
@@ -2,16 +2,20 @@
 
 class ProductsController extends \OpenApi\Core\Controller
 {
-    public function getIndexAction($code)
+    public function getIndexAction()
     {
-        /** @var ModelSettingSetting $settingModel */
-        $settingModel = $this->adminModel("setting/setting");
+        /** @var ModelCatalogProduct $productModel */
+        $productModel = $this->adminModel("catalog/product");
+        // @TODO: improve filter criteria
+        $filters = array(
+            "filter_quantity" => $this->getRequest()->getParam("quantity"),
+            "filter_status" => $this->getRequest()->getParam("status"),
+            "limit" => $this->get("config")->get("config_limit_admin"),
+            "start" => ($this->getRequest()->getParam("page", 1) - 1) * $this->get("config")->get("config_limit_admin")
+        );
+        $products = $productModel->getProducts($filters);
 
-        $config = $settingModel->getSetting($code);
-        if (empty($config)) {
-            throw new \OpenApi\Exception\NotFoundException(sprintf("'%s' adında bir config bulunamadı.", $code));
-        }
-        return new \OpenApi\Http\JsonResponse($config);
+        return new \OpenApi\Http\JsonResponse($products);
     }
 
     public function getAttributesAction($id)
